f18e2de5b976455728836c463aa02b9b2ff5f59c
samuelrock <samrock@piertopier.us>
2016-11-19 21:58:00 -0600

    Update x86_netinstall_image_prep.sh

--- platforms/x86/jessie/x86_netinstall_image_prep.sh
+++ platforms/x86/jessie/x86_netinstall_image_prep.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
 # DL firstboot script and put in in /usr/local/bin
-wget https://github.com/N4IRS/AllStar/raw/master/platforms/x86/jessie/x86_netinstall_allstar_asterisk_install.sh -O /srv/x86_netinstall_allstar_asterisk_install.sh
+wget https://github.com/samuelrock/AllStar/raw/master/platforms/x86/jessie/x86_netinstall_allstar_asterisk_install.sh -O /srv/x86_netinstall_allstar_asterisk_install.sh
 
 # DL firstboot rc.local patch and put in in /tmp
-wget https://github.com/N4IRS/AllStar/raw/master/patches/patch-x86-first-netinstall-rc.local -O /tmp/patch-x86-first-netinstall-rc.local
+wget https://github.com/samuelrock/AllStar/raw/master/patches/patch-x86-first-netinstall-rc.local -O /tmp/patch-x86-first-netinstall-rc.local
 
 # make the script executable
 chmod +x /srv/x86_netinstall_allstar_asterisk_install.sh
@@ -18,20 +18,20 @@ patch </tmp/patch-x86-first-netinstall-rc.local
 echo "rc.local modified to run x86_netinstall_allstar_asterisk_install.sh" >>/var/log/install.log
 
 # stop sshd from listening to ipv6
-wget https://github.com/N4IRS/AllStar/raw/master/patches/patch-sshd_config -O /tmp/patch-sshd_config
-cd  /etc/ssh
-patch </tmp/patch-sshd_config
-echo "removed sshd ipv6 listener" >>/var/log/install.log
+#wget https://github.com/N4IRS/AllStar/raw/master/patches/patch-sshd_config -O /tmp/patch-sshd_config
+#cd  /etc/ssh
+#patch </tmp/patch-sshd_config
+#echo "removed sshd ipv6 listener" >>/var/log/install.log
 
 # disable exim4 daemon
-wget https://github.com/N4IRS/AllStar/raw/master/patches/patch-exim4 -O /tmp/patch-exim4
+wget https://github.com/samuelrock/AllStar/raw/master/patches/patch-exim4 -O /tmp/patch-exim4
 cd /etc/default/
 patch </tmp/patch-exim4
 echo "disabled exim4 daemon" >>/var/log/install.log
 
 # Disable /etc/network/interfaces
 # This could be a simple delete file
-wget https://github.com/N4IRS/AllStar/raw/master/patches/patch-interfaces -O /tmp/patch-interfaces
+wget https://github.com/samuelrock/AllStar/raw/master/patches/patch-interfaces -O /tmp/patch-interfaces
 cd /etc/network
 patch </tmp/patch-interfaces
 echo "Disabled /etc/network/interfaces" >>/var/log/install.log
